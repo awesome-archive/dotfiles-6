3ccb33ca87b60a5e2cd4fc95c7a5a693e0b28223
zhangfan3427 <zhangfan3427@cvte.com>
2017-11-12 17:37:21 +0800

    修复bug

--- install.py
+++ install.py
@@ -3,45 +3,59 @@
 
 import os
 import os.path
+import shutil
+import json
 
 file_list = []
 HOME = os.path.expanduser('~')
 DOTFILES_HOME = os.path.join(HOME, "dotfiles")
 
 
-def _get_file_list():
+def __get_file_list():
     for i in os.listdir(DOTFILES_HOME):
         if (i.startswith('.')):
             file_list.append(i)
 
 
-def _file_path_check(file_path):
+def __file_path_check(file_path):
     if os.path.exists(file_path):
-        is_dir = os.path.isdir(file_path)
         is_replace = input("文件 %s 已存在，是否替换？(y/N)" % file_path)
         if (is_replace.lower() != 'y'):
             return False
         else:
-            if is_dir:
-                os.rmdir(file_path)
-            else:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
                 os.remove(file_path)
+            else:
+                shutil.rmtree(file_path)
         return True
 
 
-def _install():
+def __install():
     for i in file_list:
         src = os.path.join(DOTFILES_HOME, i)
         dest = os.path.join(HOME, i)
 
-        if _file_path_check(dest):
+        if __file_path_check(dest):
             os.symlink(src, dest)
             print("已创建 %s 到 %s 的软链接" % (dest, src))
 
 
+def __personalize():
+    is_home = input("当前环境是否为个人使用？(Y/n) ")
+    LISENCE_FILE = os.path.join(HOME, ".vim/pluginconfig/license.txt")
+    with open(LISENCE_FILE, "r") as fin:
+        license = json.loads(fin.read())
+    with open(LISENCE_FILE, "w") as fout:
+        if is_home.lower() == 'n':
+            fout.write(license["work"])
+        else:
+            fout.write(license["personal"])
+
+
 def main():
-    _get_file_list()
-    _install()
+    __get_file_list()
+    __install()
+    __personalize()
 
 
 if __name__ == "__main__":
