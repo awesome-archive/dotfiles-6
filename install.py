c92f520b450b2539c838d28802de9acd3288f63d
Zhang Fan <zhangfan3427@cvte.com>
2018-02-10 19:30:34 +0800

    更新安装脚本

--- install.py
+++ install.py
@@ -4,17 +4,20 @@
 import os
 import os.path
 import shutil
-import json
+import time
 
 file_list = []
+ignore_list = [".git", ".gitignore"]
+template_list = ["license", "gitconfig"]
 HOME = os.path.expanduser('~')
 DOTFILES_HOME = os.path.join(HOME, "dotfiles")
 
 
 def __get_file_list():
     for i in os.listdir(DOTFILES_HOME):
-        if (i.startswith('.')):
+        if (i.startswith('.') and i not in ignore_list):
             file_list.append(i)
+
 
 def __file_path_check(file_path):
     if os.path.exists(file_path):
@@ -39,23 +42,32 @@ def __install():
             print("已创建 %s 到 %s 的软链接" % (dest, src))
 
 
-def __personalize():
-    is_home = input("当前环境是否为个人使用？(Y/n) ")
-    LISENCE_INFO_FILE = os.path.join(DOTFILES_HOME, "LICENSE")
-    with open(LISENCE_INFO_FILE, "r") as fin:
-        license = json.loads(fin.read())
-    LISENCE_FILE = os.path.join(HOME, ".vim/pluginconfig/license.txt")
-    with open(LISENCE_FILE, "w") as fout:
-        if is_home.lower() == 'n':
-            fout.write(license["work"])
-        else:
-            fout.write(license["personal"])
+# 实例化模板
+def __instantiation():
+    name = input("姓名: ")
+    corporation = input("组织名: ")
+    mail = input("邮箱: ")
+    year = time.localtime(time.time()).tm_year
+    for i in template_list:
+        src = os.path.join(DOTFILES_HOME, i)
+        dest = os.path.join(HOME, "." + i)
+
+        with open(src, 'r') as fin:
+            filedata = fin.read()
+
+        filedata = filedata.replace('`Name`', name)
+        filedata = filedata.replace('`Corporation`', corporation)
+        filedata = filedata.replace('`Mail`', mail)
+        filedata = filedata.replace('`Year`', str(year))
+
+        with open(dest, 'w') as fout:
+            fout.write(filedata)
 
 
 def main():
     __get_file_list()
     __install()
-    __personalize()
+    __instantiation()
 
 
 if __name__ == "__main__":
